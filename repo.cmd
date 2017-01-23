f59767bf98625314dbd0b9a97eed2371dc20d52d
Johan Lindh <johan@linkdata.se>
2017-01-23 11:41:59 +0100

    Use Python27 if installed, and quote %~dp0

--- repo.cmd
+++ repo.cmd
@@ -1,1 +1,6 @@
-@call python %~dp0\repo %*
+@echo off
+setlocal
+set PYTHON_EXE=python
+if exist C:\Python27\python.exe set PYTHON_EXE=C:\Python27\python.exe
+call %PYTHON_EXE% "%~dp0\repo" %*
+endlocal
